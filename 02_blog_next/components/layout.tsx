780681dc082987c9488d6ead3ba8d9c1e2a85b77
OlgaBouin <olga.bouin@adeo.com>
2021-05-17 11:47:13 +0200

    S9 D1 next v0 initialization for exercises

---
+++ 02_blog_next/components/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const Layout: React.FC = ({ children }) => {
+  return (
+    <div>
+      <span>This would be displayed at the top</span>
+      <article>{children}</article>
+    </div>
+  );
+};
---
+++ 02_blog_next/pages/first-app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FirstApp = () => {
+  return (
+    <div>
+      <h2>First App</h2>
+    </div>
+  );
+};
+
+export default FirstApp;
